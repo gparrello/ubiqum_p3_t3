06ac60635bc5867af09d7d3872a94f9341364590
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-02-11 15:47:17 +0100

    add comments and check list for unique values

--- data.R
+++ data.R
@@ -9,18 +9,23 @@ files <- c(
 )
 dt <- c()
 longdt <- c()
+check_list <- c()
 for(s in names(files)){
   
   d <- fread(files[[s]])
   
+  # set 100 to NAs
   for(j in seq_along(d[,1:520])) {
     set(d, i = which(d[[j]] == 100), j = j, value = NA)
   }   
   
+  # melt into long format
   melt_ids <- colnames(d[,521:529])
   l <- melt(d, id.vars = melt_ids)
   names(l)[names(l) == 'variable'] <- 'WAP'
   longdt[[s]] <- l
+  
+  check_list[[s]] <- apply(longdt[[s]][, c(3:8, 10)], 2, unique)
   
   # remove all rows that only contain +100 in WAP signal
   # keeprows <- apply(dt[[s]][,1:520], 1, function(x) length(unique(x[!is.na(x)])) != 1)  # criteria for selecting 100 and not all rows with all -40 for example?
