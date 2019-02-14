c2d54a7a21df1ee7e3c003d1aeb0fcd96300cbed
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-02-14 18:46:11 +0100

    shorten top waps function

--- data_functions.R
+++ data_functions.R
@@ -1,6 +1,5 @@
 find_top_waps <- function(x, names = FALSE, k = 1){
-  x <- sort(x, na.last = TRUE, decreasing = TRUE)
+  x <- order(x, na.last = TRUE, decreasing = TRUE)[1:k]
   if (names == TRUE) x <- names(x)
-  x <- x[1:k]
   return(x)
 }
