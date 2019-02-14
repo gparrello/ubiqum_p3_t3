55d5e51a16adcbfa2af36e6ccc2a7285502ca54e
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-02-14 18:47:34 +0100

    add comments

--- data_functions.R
+++ data_functions.R
@@ -1,4 +1,9 @@
 find_top_waps <- function(x, names = FALSE, k = 1){
+  
+  # from:
+  # - https://stackoverflow.com/questions/6827299/r-apply-function-with-multiple-parameters
+  # - https://stackoverflow.com/questions/17619782/how-to-find-the-largest-n-elements-in-a-list-in-r
+  
   x <- order(x, na.last = TRUE, decreasing = TRUE)[1:k]
   if (names == TRUE) x <- names(x)
   return(x)
