9e7007b691c84f5886c2ba24d6c3e70a298a8d2f
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-02-20 15:06:47 +0100

    add parallel processing and caret option

--- model_functions.R
+++ model_functions.R
@@ -1,22 +1,24 @@
 pacman::p_load(
-  "readr",
+  # "readr",
+  "doParallel",
   "caret",
+  "xgboost",
   "randomForest",
   "e1071",
-  "gbm",
-  "doParallel"
+  "gbm"
 )
 
-do_modeling <- function(x, y){
+do_modeling <- function(x, y, caret = FALSE){
 
   model <- c()
-  cl <- makeCluster(detectCores() - 2)
-  registerDoParallel(cl)
+  cores <- detectCores() - 2
+  cluster <- makeCluster(cores, type = "FORK")
+  registerDoParallel(cluster)
   
   
   # Train Control
   cvFoldNum <- 10
-  cvRepeatNum <- 5
+  cvRepeatNum <- 3
   cvFolds <- createMultiFolds(
     y,
     k = cvFoldNum,
@@ -29,37 +31,58 @@ do_modeling <- function(x, y){
     allowParallel = TRUE
   )
   
-  # Get the best mtry
-  bestmtry <- tuneRF(
-    x,
-    y,
-    ntreeTry = 100,
-    stepFactor = 2,
-    improve = 0.05,
-    trace = TRUE,
-    plot = FALSE,
-    trControl = fitControl
-  )
+  if (caret == TRUE) {
+      
+    # Train
+    tuneLength <- 1
+    allmodels <- list(
+      # "svmLinear",
+      # "svmRadial",
+      # "svmPoly",
+      # "lm",
+      # "knn",
+      # "gbm",
+      # "xgbTree",
+      "rf"
+    )
+    
+    for(m in allmodels){
+      model[[m]] <- train(
+        x = x,
+        y = y,
+        # data = trainingSet, # the problem is here!
+        method = m,
+        trControl = fitControl,
+        tuneLength = tuneLength
+      )
+    }
   
-  # Train a random forest using that mtry
-  model[["rf"]] <- randomForest(
-    x = x,
-    y = y,
-    importance = TRUE,
-    method = "rf",
-    ntree = 100,
-    mtry = bestmtry[[1]],
-    trControl = fitControl
-  )
+  } else {
+    
+    # Train RF
+    bestmtry <- tuneRF(
+      x, y,
+      ntreeTry = 100,
+      stepFactor = 2,
+      improve = 0.05,
+      trace = TRUE,
+      plot = FALSE,
+      trControl = fitControl
+    )
+    
+    model[["rf"]] <- randomForest(
+      y = y,
+      x = x,
+      importance = TRUE,
+      method = "rf",
+      ntree = 100,
+      mtry = bestmtry[[1]],
+      trControl = fitControl
+    )
+
+  }
   
-  # Train a svm
-  # model[["svm"]] <- svm(y = y, x = x, trControl = fitControl)
-  
-  # Train a gbm
-  # model[["gbm"]] <- gbm(x = x, y = y)
-  
-  
-  stopCluster(cl)
+  stopCluster(cluster)
   return(model)
 
 }
