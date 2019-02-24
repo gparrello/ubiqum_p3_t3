f0b83bf879b5a7c9c155d3000af2f8e9a5467f82
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-02-24 18:22:57 +0100

    complete final predict script

--- predict.R
+++ predict.R
@@ -1,26 +1,41 @@
 source("./data.R")
 
-labels <- c(
-  "building"#,
-  # "floor",
-  # "long",
-  # "lat"
-)
-models <- c()
-
-for (l in labels) {
-  load(paste("./best/", l, ".rda", sep=""))
-  models[[l]] <- model
-}
-
-common_columns <- Reduce(
-  intersect, list(
-    colnames(dt[["train"]]),
-    colnames(dt[["validation"]]),
-    colnames(dt[["test"]])
-  )
-)
-
 data <- fread("./data/testData.csv")
 waps <- grep("WAP", names(data), value=TRUE)
-prediction <- predict(models[["building"]]$rf, data[,..waps])
+
+labels <- c(
+  "building",
+  "floor",
+  "long",
+  "lat",
+  "long2"
+)
+variables <- c(
+  "BUILDINGID",
+  "FLOOR",
+  "LONGITUDE",
+  "LATITUDE",
+  "LONGITUDE"
+)
+names(variables) <- labels
+models <- c()
+prediction <- data.frame()
+
+for (l in labels) {
+  print(l)
+  load(paste("./best/", l, ".rda", sep=""))
+  if (!(prod(dim(prediction)) == 0)) {
+    d <- cbind(data[,..waps], prediction)
+  } else {
+    d <- data[,..waps]
+  }
+  p <- predict(model$rf, d)
+  p <- as.data.frame(p)
+  colnames(p) <- variables[[l]]
+  # browser()
+  if (!(prod(dim(prediction)) == 0)) {
+    prediction <- cbind(prediction, p)
+  } else {
+    prediction <- p
+  }
+}
