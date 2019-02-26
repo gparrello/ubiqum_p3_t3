59d3450dbc0e8f1e3cb748f3a48cd7e164144fd1
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-02-26 21:27:22 +0100

    remove building as predicted column

--- experiment.R
+++ experiment.R
@@ -4,14 +4,13 @@ pacman::p_load(
 
 get_data <- function(x){
   df <- read.csv(file=paste(folder, x, sep=""))
-  # df$who <- x
   if (df$X[[1]] == 0) df$X <- df$X + 1
   df$X <- as.integer(df$X)
-  df$BUILDINGID <- as.factor(df$BUILDINGID)
   df$FLOOR <- as.factor(df$FLOOR)
   df$LONGITUDE <- as.numeric(df$LONGITUDE)
   df$LATITUDE <- as.numeric(df$LATITUDE)
-  df <- df[c("X", "BUILDINGID", "FLOOR", "LONGITUDE", "LATITUDE")]
+  df <- df[c("X", "FLOOR", "LONGITUDE", "LATITUDE")]
+  df$who <- x
   return(df)
 }
 
@@ -29,16 +28,14 @@ list_df <- lapply(
 )
 names(list_df) <- prediction_files
 binded_df <- do.call(rbind, list_df)
-binded_df$BUILDINGID <- as.factor(binded_df$BUILDINGID)
 binded_df$FLOOR <- as.factor(binded_df$FLOOR)
 
 grouped_df <- binded_df %>%
   group_by(X) %>%
   summarise(
-    BUILDINGID_avg = get_mode(BUILDINGID),
-    FLOOR_avg = get_mode(FLOOR),
-    LONGITUDE_avg = mean(LONGITUDE),
-    LATITUDE_avg = mean(LATITUDE)
+    FLOOR = get_mode(FLOOR),
+    LONGITUDE = mean(LONGITUDE),
+    LATITUDE = mean(LATITUDE)
   )
 
 write.csv(grouped_df, file = paste(folder, "final.csv"), row.names = FALSE)
