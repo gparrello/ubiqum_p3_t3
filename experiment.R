02336affce79016208aa30d4cc4ff4dd839d8ea9
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-02-26 21:27:32 +0100

    add plots

--- experiment.R
+++ experiment.R
@@ -39,3 +39,44 @@ grouped_df <- binded_df %>%
   )
 
 write.csv(grouped_df, file = paste(folder, "final.csv"), row.names = FALSE)
+grouped_df$who <- "combined"
+full_df <- rbind(binded_df, grouped_df)
+
+ggplot(data = full_df) +
+  aes(x = LATITUDE, fill = who) +
+  geom_histogram(bins = 30) +
+  theme_minimal() +
+  facet_grid(cols=vars(who))
+
+ggplot(data = full_df) +
+  aes(x = LONGITUDE, fill = who) +
+  geom_histogram(bins = 30) +
+  theme_minimal() +
+  facet_grid(cols=vars(who))
+
+ggplot(data = full_df) +
+  aes(x = FLOOR, fill = who) +
+  geom_bar() +
+  theme_minimal() +
+  facet_grid(cols=vars(who))
+
+ggplot(data = full_df) +
+  aes(x = LONGITUDE, y = LATITUDE, color = who) +
+  geom_point() +
+  theme_minimal() +
+  facet_grid(cols=vars(FLOOR), rows=vars(who))
+
+ggplot(data = full_df) +
+  aes(x = LONGITUDE, y = LATITUDE, color = who) +
+  geom_point() +
+  theme_minimal() +
+  facet_wrap(vars(FLOOR))
+
+# library("rbokeh")
+# figure() %>%
+#   ly_points(
+#     data = full_df,
+#     x = LONGITUDE,
+#     y = LATITUDE,
+#     color = who
+#   )
